d9822d030cf01425763880c803af0aef36020b42
Scott Harris <scottharris86@me.com>
2019-12-31 10:04:54 -0500

    Initial Commit

---
+++ Youtube/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Youtube
+//
+//  Created by scott harris on 12/31/19.
+//  Copyright © 2019 scott harris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
